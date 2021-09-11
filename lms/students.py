1f79b5c094b04772a0aa14a96b109de563dafab8
Yevheniia Mishchishyna <janekaraim@gmail.com>
2021-09-11 15:53:58 +0300

    exception handling

--- lms/students.py
+++ lms/students.py
@@ -4,14 +4,32 @@ STUDENTS = []
 
 TEST_STUDENTS = [
     ['Mary', 'D', 'mail@mail.com', '19', 'Huston', 'F'],
-    ['John', 'S', 'new_mail@mail.com', '21', 'London', 'M']
+    ['John', 'S', 'new_mail@mail.com', '21', 'London', 'M'],
+    ['Andy', 'H', 'more_mail@mail.com', 'sexteen', 'Brighton', 'M']
 ]
 
 def add_student():
     student = {}
     for field in student_fields:
         student[field] = input('Enter {}\t'.format(field))
+        if field == 'age':
+            try:
+                int(student['age'])
+            except:
+                student['age'] = input('Enter age as number\t')
     STUDENTS.append(student)
+
+def calculate_avg_age():
+    try:
+        total_age = 0
+        for student in STUDENTS:
+            total_age += int(student['age'])
+        avgerage_age = total_age / len(STUDENTS)
+        print('Average age is {}'.format(avgerage_age))
+    except ValueError:
+        print('Cannot calculate average age')
+    except Exception as e:
+        print(str(e))
 
 def print_student(student):
     for field in student:
@@ -19,15 +37,14 @@ def print_student(student):
 
 def load_students():
     for test_student in TEST_STUDENTS:
-        student = {}
-        for index in range(len(student_fields)):
-            student[student_fields[index]] = test_student[index]
-        STUDENTS.append(student)
+        STUDENTS.append(dict(zip(student_fields, test_student)))
 
 while True:
     action = input('Desired action:\t')
     if action == 'add':
         add_student()
+    elif action == 'avg_age':
+        calculate_avg_age()
     elif action == 'load':
         load_students()
     elif action == 'print':
