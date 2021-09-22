d71171fa98d4c0ff54507fc49ae4a9472544eb3b
Yevheniia Mishchishyna <janekaraim@gmail.com>
2021-09-22 21:06:48 +0300

    lesson 11

--- animals.py
+++ animals.py
@@ -16,6 +16,9 @@ class Animal:
 
 class Cat(Animal):
 
+    def __eq__(self, other):
+        return self.name == other.name
+
     @staticmethod
     def voice():
         print('MEOW!!!!!!!')
@@ -27,18 +30,77 @@ class Kitten(Cat):
         super().__init__(name)
         self.age = age
 
+    @staticmethod
+    def voice():
+        print('FFFFF!!!')
+
 
 class Dog(Animal):
 
-    def voice(self):
-        print('Bark! My name is {}'.format(self.name))    
+    weight = 0
+    TOTAL_COUNT = 0
 
+    def __init__(self, name='Tuzik', weight=10):
+        super().__init__(name)
+        self.weight = weight
+        Dog.TOTAL_COUNT += 1
+
+    def __eq__(self, other):
+        if hasattr(other, 'weight'):
+            return self.weight == other.weight
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def voice(self):
+        print('Bark! My name is {}'.format(self.name)) 
+
+    @classmethod
+    def get_total_count(cls):
+        return cls.TOTAL_COUNT
+
+    @classmethod
+    def from_dict(cls, dict_data):
+        obj = cls()  
+        for key in dict_data:
+            setattr(obj, key, dict_data[key])
+        return obj
 
 class CatDog(Dog, Cat):
 
     def voice(self):
         print('Meow! Bark!')
 
-print(dir(Cat))
-kitty = Kitten('Kitty', '1 month')
-kitty.voice()
+
+if __name__ == '__main__':
+    dog1 = Dog()
+#    print(dog1)
+#    print(dog1.weight)
+    dog_data = {'name': 'Joy', 'weight': 15}
+    dog2 = Dog.from_dict(dog_data)
+    print(dog2)
+    print(dog2.weight)
+#    cat = Cat('Murzik')
+#    print(dog1 == cat)
+#    print(cat == dog1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
