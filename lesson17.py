abf595a694f0ce35ea1408a3e3d499bd6c5c0415
Yevheniia Mishchishyna <janekaraim@gmail.com>
2021-10-09 16:03:01 +0300

    add_location task method

--- lesson17.py
+++ lesson17.py
@@ -1,9 +1,12 @@
+import googlemaps
+
 class Task:
 
     def __init__(self, title):
         self.done = False
         self.title = title
         self._priority = 1
+        self.location = None
 
     def __str__(self):
         return self.title
@@ -18,6 +21,21 @@ class Task:
             self._priority = value
         else:
             raise ValueError('Priority value is out of range')
+
+    def add_location(self):
+        place_lookup = input('Enter location name: \t')
+        gmaps = googlemaps.Client(
+            key='AIzaSyDZUTx1HWrOcNDng1V7-smaaHTBSobrw0I')
+        place = gmaps.find_place(
+            place_lookup,
+            'textquery',
+            fields=['geometry/location', 'name', 'place_id']
+        )
+        self.location = {
+            'coordinates': place['candidates'][0]['geometry']['location'],
+            'name': place['candidates'][0]['name'],
+            'google_id': place['candidates'][0]['place_id']
+        }
 
 class Dashboard:
 
