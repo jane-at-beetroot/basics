8429c69666004eb8b15588519383ee45754ea132
Yevheniia Mishchishyna <janekaraim@gmail.com>
2021-10-20 21:10:23 +0300

    lesson 22

---
+++ lesson22.py
@@ -0,0 +1,50 @@
+def find_element(array: list, element: int) -> int:
+    for index in range(len(array)):
+        if array[index] == element:
+            return index
+    return -1 
+
+test_arr = [1, 2, 3] element = 2
+test_arr = list(range(10000)) element = 7854
+
+'''
+O(1) - константна складність
+'''
+test_dict = {'1': 1, '2': 2, '3': 3}
+test_dict['2']
+
+def compare(a_list, b_list):
+    return a_list[0] == b_list[0]
+
+
+'''
+O(log n) - логарифмічна складність
+'''
+5, 8, 6, 7
+
+'''
+O(n**k, k < 1) - сублінійна складність
+'''
+
+'''
+O(n) - лінійна складність
+'''
+1, 2, 3, ..., 10
+
+'''
+O(n*log(n)) - лінійно-логарифмічна
+'''
+
+'''
+O(n**2) - квадратична
+O(n**3) - кубічна
+'''
+
+'''
+O(10**n) - експоненційна складність
+'''
+
+
+
+
+
