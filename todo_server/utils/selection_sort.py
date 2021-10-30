9927b59d2d62f2fac635bf6f653242c42308b2ec
Yevheniia Mishchishyna <janekaraim@gmail.com>
2021-10-30 11:17:59 +0300

    exercises from euler project

---
+++ todo_server/utils/selection_sort.py
@@ -0,0 +1,7 @@
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i+1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j       
+        array[i], array[min_index] = array[min_index], array[i]
